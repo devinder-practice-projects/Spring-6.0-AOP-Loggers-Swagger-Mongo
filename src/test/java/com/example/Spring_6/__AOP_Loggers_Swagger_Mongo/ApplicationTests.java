375edf3ee69bac027c4957618454ad20cc202b6b
Devinder <devinderwadhwa20@gmail.com>
2025-09-29 23:14:03 +0530

    Created Project

---
+++ src/test/java/com/example/Spring_6/__AOP_Loggers_Swagger_Mongo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_6.__AOP_Loggers_Swagger_Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
